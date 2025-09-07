f5a21d24c6a0c1e42eb6dfbfc283c7f795ba2d60
TOMORI-TETSUYA <x20a_zgmf_strike@yahoo.co.jp>
2025-09-07 14:14:18 +0900

    アクターを描くまで

--- numbers.py
+++ numbers.py
@@ -12,8 +12,24 @@ for dot in range(0, 10):
 
     actor = Actor("dot")
 
-    actor = .pos = randint(20, WIDTH - 20), \
+    actor.pos = randint(20, WIDTH - 20), \
 
     randint(20, HEIGHT - 20)
 
     dots.append(actor)
+
+def draw():
+
+    screen.fill("black")
+
+    number = 1
+
+    for dot in dots:
+
+        screen.draw.text(str(number), \
+
+                            (dot.pos[0], dot.pos[1] + 12))
+
+        dot.draw()
+
+        number = number + 1
