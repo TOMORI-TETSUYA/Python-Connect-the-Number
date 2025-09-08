e260e0a2a55c70252990760a12f7974cab9b16e4
TOMORI-TETSUYA <x20a_zgmf_strike@yahoo.co.jp>
2025-09-08 18:24:44 +0900

    ゲームプログラム

--- numbers.py
+++ numbers.py
@@ -36,9 +36,9 @@ def on_mouse_down(pos):
     global next_dot
     global lines
 
-    if dots[next_dot].coiiidepoint(pos):
+    if dots[next_dot].collidepoint(pos):
         if next_dot:
-            lines.append((dots[next_dot - 1].pos, dots[next_dot].pos))
+            lines.append((dots[next_dot - 1].pos, dots[next_dot].pos))  
         next_dot = next_dot + 1
 
     else:
